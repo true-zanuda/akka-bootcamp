84c0e39cc074b90b3d91d11d60c7c417010a36b5
Eugene Griaznov <evgeniy.gryaznov@gmail.com>
2019-12-01 07:25:57 +0700

    Do lesson 1.6

--- src/Unit-1/DoThis/TailActor.cs
+++ src/Unit-1/DoThis/TailActor.cs
@@ -15,19 +15,6 @@ namespace WinTail
         {
             _reporterActor = reporterActor;
             _filePath = filePath;
-
-            // start watching file for changes
-            _observer = new FileObserver(Self, Path.GetFullPath(_filePath));
-            _observer.Start();
-
-            // open the file stream with shared read/write permissions
-            // (so file can be written to while open)
-            _fileStream = new FileStream(Path.GetFullPath(_filePath), FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-            _fileStreamReader = new StreamReader(_fileStream, Encoding.UTF8);
-
-            // read the initial contents of the file and send it to console as first msg
-            var text = _fileStreamReader.ReadToEnd();
-            Self.Tell(new InitialRead(_filePath, text));
         }
 
         protected override void OnReceive(Object message)
@@ -55,11 +42,38 @@ namespace WinTail
             }
         }
 
+        protected override void PreStart()
+        {
+            base.PreStart();
+
+            // start watching file for changes
+            _observer = new FileObserver(Self, Path.GetFullPath(_filePath));
+            _observer.Start();
+
+            // open the file stream with shared read/write permissions
+            // (so file can be written to while open)
+            _fileStream = new FileStream(Path.GetFullPath(_filePath), FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            _fileStreamReader = new StreamReader(_fileStream, Encoding.UTF8);
+
+            // read the initial contents of the file and send it to console as first msg
+            var text = _fileStreamReader.ReadToEnd();
+            Self.Tell(new InitialRead(_filePath, text));
+        }
+
+        protected override void PostStop()
+        {
+            _observer.Dispose();
+            _observer = null;
+            _fileStreamReader.Close();
+            _fileStreamReader.Dispose();
+            base.PostStop();
+        }
+
         private readonly String _filePath;
         private readonly IActorRef _reporterActor;
-        private readonly FileObserver _observer;
-        private readonly Stream _fileStream;
-        private readonly StreamReader _fileStreamReader;
+        private FileObserver _observer;
+        private Stream _fileStream;
+        private StreamReader _fileStreamReader;
 
         #region Message types
 
